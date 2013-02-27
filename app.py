52127c4c379d74939c39d92d1f50eae6779f291c
Alykhan Kanji <github@alykhan.com>
2013-02-27 15:04:05 -0500

    Add landing page for root, custom page for HTTP 400, now using 'get' endpoint to handle API requests.

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
 import json, os, getSchedule
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, jsonify, render_template, abort
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def root():
+	return render_template('index.html')
+
+@app.route('/get')
+def get():
 	stop = request.args.get('stop', '')
 	if stop is '' or not stop.isdigit():
 		abort(400)
 	schedule = getSchedule.parseSchedule(getSchedule.getRides(stop))
 	return jsonify(schedule)
+
+@app.errorhandler(400)
+def bad_request(error):
+	return render_template('bad_request.html'), 400
 
 if __name__ == "__main__":
 	# Bind to PORT if defined, otherwise default to 5000.
